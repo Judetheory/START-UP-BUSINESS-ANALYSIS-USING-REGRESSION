af77db70e176a34a46791eb3334e682950196abc
Jude Tochukwu Ogbozor <judetheory@gmail.com>
2022-05-30 11:55:18 +0100

    app run

--- flake_app.py
+++ flake_app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-app=Flask(__name__)
-@app.route('/')
-def Home:
-    
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hello world!"
+app.run(port=5000)
